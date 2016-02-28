63b3d781c94aebbbed5fcecc96b4efd23425c190
Thijs Noordzij <thijs.noordzij@gmail.com>
2016-02-28 22:22:22 +0100

    SPSS sytax
    Added SPSS syntax that does the trick

--- MIRROR data manipulation.R
+++ MIRROR data manipulation.R
@@ -19,3 +19,10 @@ data <- read_spss("C:/Users/Thijs/Documents/MIRROR/data/mirror_formatie_ tm2014.
 # 3. Create date variable out of GEBDAT
 
 # 4. Fill missing GEBDAT is known within same id_2015
+# SPSS syntax used to fill missing GEBDAT and GESLACHT: 
+#   * Vullen missende geboortedatum en geslacht als er wel een geboortedatum/geslacht bekend is binnen een id_2015. 
+# SORT CASES BY id_2015(A) Geboortedatum(D).
+# IF (id_2015 = LAG(id_2015)) AND (SYSMIS(Geboortedatum) ) Geboortedatum = LAG(Geboortedatum). 
+# SORT CASES BY id_2015(A) GESLACHT(D).
+# IF (id_2015 = LAG(id_2015)) AND (SYSMIS(GESLACHT) ) GESLACHT = LAG(GESLACHT). 
+# EXECUTE.
