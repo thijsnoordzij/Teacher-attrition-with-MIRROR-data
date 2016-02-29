7e55382871429f271053f05537455125fd0186b3
Thijs Noordzij <thijs.noordzij@gmail.com>
2016-02-29 23:06:15 +0100

    Playing with plyr library

--- MIRROR data manipulation.R
+++ MIRROR data manipulation.R
@@ -28,3 +28,28 @@ data <- read_spss("C:/Users/Thijs/Documents/MIRROR/data/mirror_formatie_ tm2014.
 # EXECUTE.
 
 # perhaps the zoo library has good options to handle missing data?
+
+library(plyr) # helpfull tutorial of plyr: http://www.r-bloggers.com/a-fast-intro-to-plyr-for-r/
+
+# Test with small set of total data (50k of the 6,5M, <1%)
+subdata <- data[1:50001,]
+
+
+temptab <- ddply(subdata, c("id_2015", "GEBDAT"), function(df)max(df$GEBDAT)) # for every id_2015, if one of the GEBDAT is NA, this returns NA for the whole id_2015
+temptab2 <- ddply(subdata, c("id_2015", "GEBDAT"), function(df)max(df$GEBDAT,na.rm = TRUE)) # for every id_2015, if one of the GEBDAT is NA, this returns the maximum. If all GEBDAT is NA, this returns -Inf for the whole id_2015
+
+head(temptab)
+
+x <- temptab[,"GEBDAT"] - temptab[,"V1"]
+
+temptab <- cbind(temptab, x)
+
+sum(is.na(temptab[,"x"]))
+
+tmp <- count(data1[,"id_2015"]) # 4548 observations, while temptab has 4550 oberservations. That means temptab shows id_2015's with more than one GEBDAT more than once. 
+
+y <- c(1,2,3,NA)
+z <- c(NA, NA)
+max(y) # returns NA
+max(y,na.rm = TRUE) # returns 3
+max(z,na.rm = TRUE) # returns -Inf
